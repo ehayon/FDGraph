dbf98516babf54d8e6edbb07ae88eb1bbfc40dd5
Ethan Hayon <ehayon@gmail.com>
2012-03-31 18:41:31 -0400

    added Graph

--- Graph.js
+++ Graph.js
@@ -1,7 +1,26 @@
-function Node(x, y) {
-	Circle.call(this, x, y, 30);
+function Graph(canvas_object) {
+	this.nodes = [];
+	this.connections = [];
+	this.canvas = canvas_object;
+	// add elements to the canvas object
+}
+Graph.prototype.addNode = function(node) {
+	this.nodes.push(node);
+	this.canvas.add(node);
+}
+Graph.prototype.addConnection = function(connection) {
+	this.connections.push(connection);
+	this.canvas.add(connection);
+}
+
+function Node() {
+	this.x = Math.floor(Math.random()*1000);
+	this.y = Math.floor(Math.random()*1000);
+	console.log("("+this.x+", "+this.y+")");
+	Circle.call(this, this.x, this.y, 30);
 }
 Node.prototype = Object.create(new Circle());
+
 
 function Connection(node_a, node_b) {
 	Line.call(this, node_a.x, node_a.y, node_b.x, node_b.y);
@@ -9,6 +28,7 @@ function Connection(node_a, node_b) {
 	this.b = node_b;
 	this.line = new Line(node_a.x, node_b.y);
 	this.draggable = false;
+	this.force = 0;
 }
 Connection.prototype = Object.create(new Line());
 // we need to override the draw method so it updates on a redraw
--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
 	var can = document.getElementById("can");
 	var myCanvas = new Canvas(can);
-	
+	var myGraph = new Graph(myCanvas);
 	// create some nodes and connections
-	var node1 = new Node(300, 200);
-	var node2 = new Node(200, 500);
-	var node3 = new Node(400, 200);
+	var node1 = new Node();
+	var node2 = new Node();
+	var node3 = new Node();
 	var connection1 = new Connection(node1, node2);
 	var connection2 = new Connection(node2, node3);
 	var connection3 = new Connection(node1, node3);
+
+	myGraph.addConnection(connection1);
+	myGraph.addConnection(connection2);
+	myGraph.addConnection(connection3);
+
+	myGraph.addNode(node1);
+	myGraph.addNode(node2);
+	myGraph.addNode(node3);
+
 	
-	// add elements to the canvas object
-	myCanvas.add(connection1);	
-	myCanvas.add(connection2);
-	myCanvas.add(connection3);
-	myCanvas.add(node1);
-	myCanvas.add(node2);
-	myCanvas.add(node3);
+
 
 });
