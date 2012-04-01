bafaf164593803f7fa97efc75eee9bc7a1927a2c
Ethan Hayon <ehayon@gmail.com>
2012-03-31 23:15:54 -0400

    added more nodes

--- main.js
+++ main.js
@@ -6,18 +6,47 @@ $(document).ready(function() {
 	var node1 = new Node();
 	var node2 = new Node();
 	var node3 = new Node();
+	var node4 = new Node();
+	var node5 = new Node();
+	var node6 = new Node();
+	var node7 = new Node();
+	var node8 = new Node();
+	
 	var connection1 = new Connection(node1, node2);
 	var connection2 = new Connection(node2, node3);
 	var connection3 = new Connection(node1, node3);
+	var connection4 = new Connection(node1, node4);
+	var connection5 = new Connection(node1, node5);
+	var connection6 = new Connection(node1, node6);
+	var connection7 = new Connection(node2, node7);
+	var connection8 = new Connection(node2, node8);
+	var connection9 = new Connection(node3, node8);
+	var connection10 = new Connection(node4, node7);
+	
 
 	myGraph.addConnection(connection1);
 	myGraph.addConnection(connection2);
 	myGraph.addConnection(connection3);
+	myGraph.addConnection(connection4);
+	myGraph.addConnection(connection5);
+	myGraph.addConnection(connection6);
+	myGraph.addConnection(connection7);
+	myGraph.addConnection(connection8);
+	myGraph.addConnection(connection9);
+	myGraph.addConnection(connection10);
+
+	
 
 	myGraph.addNode(node1);
 	myGraph.addNode(node2);
 	myGraph.addNode(node3);
+	myGraph.addNode(node4);
+	myGraph.addNode(node5);
+	myGraph.addNode(node6);
+	myGraph.addNode(node7);
+	myGraph.addNode(node8);
 
+	
 	
 
 
