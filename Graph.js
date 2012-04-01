b1b60e88465c0153b670d06ca6f0e3a7eb61cabf
Ethan Hayon <ehayon@gmail.com>
2012-03-31 23:10:51 -0400

    improved spring

--- Graph.js
+++ Graph.js
@@ -2,9 +2,9 @@ function Graph(canvas_object) {
 	this.nodes = [];
 	this.connections = [];
 	this.canvas = canvas_object;
-	this.damping = .0000001;
+	this.damping = .00001;
 	var myGraph = this;
-	setInterval(function() { myGraph.checkRedraw() }, 10);
+	setInterval(function() { myGraph.checkRedraw() }, 30);
 	// add elements to the canvas object
 	this.timestep = 0;
 	this.kineticenergy = 0;
@@ -20,19 +20,34 @@ Graph.prototype.addConnection = function(connection) {
 Graph.prototype.checkRedraw = function() {
 	// compute the force on each connection
 	// only update if net force is greater than threshold
+	
 	for(var i = 0; i < this.nodes.length; i++) {
 		var node = this.nodes[i];
+		node.netforcex = 0;
+		node.netforcey = 0;
 		for(var j = 0; j < this.connections.length; j++) {
 			var con = this.connections[j];
 			if(con.a == node || con.b == node) {
 				var other_node = (con.a == node) ? con.b : con.a;
 				var distance = Math.sqrt(((node.x - other_node.x)*(node.x - other_node.x)) + ((node.y - other_node.y)*(node.y - other_node.y)));
-				node.netforcex += (-.0005) * (node.x - other_node.x);
-				node.netforcey += (-.0005) * (node.y - other_node.y);
+				
+				//node.netforcex += ((node.charge*other_node.charge)/(distance * distance))*Math.cos((other_node.x - node.x)/(distance));
+				//node.netforcey += ((node.charge*other_node.charge)/(distance * distance))*Math.sin((other_node.x - node.x)/(distance));
+				var dist = Math.max(distance, 1);
+				var force = .1 * (distance - 100);
+				var angle = Math.sin((other_node.y - node.y) / distance);
+				
+				
+				node.netforcex += 1 * force * Math.sin((other_node.x - node.x)/distance);
+				node.netforcey += 1 * force * Math.sin((other_node.y - node.y)/distance);
+
+				node.velocityx = (node.velocityx + this.timestep * node.netforcex) * this.damping;
+				node.velocityy = (node.velocityy + this.timestep * node.netforcey) * this.damping;
+
+
 			}
 		}
-		node.velocityx = (node.velocityx + this.timestep * node.netforcex) * this.damping;
-		node.velocityy = (node.velocityy + this.timestep * node.netforcey) * this.damping;
+
 	
 		node.x += node.velocityx * this.timestep;
 		node.y += node.velocityy * this.timestep;
@@ -48,7 +63,7 @@ function Node() {
 	this.y = Math.floor(Math.random()*1000);
 	console.log("("+this.x+", "+this.y+")");
 	Circle.call(this, this.x, this.y, 30);
-	this.charge = 1;
+	this.charge = 40;
 	this.mass = 1;
 	this.velocityx = 0;
 	this.velocityy = 0;
