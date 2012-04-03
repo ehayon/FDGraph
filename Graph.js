bd66be9231e1d045fc3ec53824371aeeab7923a9
Ethan Hayon <ehayon@gmail.com>
2012-04-03 10:05:39 -0400

    organize the force calculations into multiple functions

--- Graph.js
+++ Graph.js
@@ -1,35 +1,36 @@
-function Graph(canvas_object) {
+function Graph(c) {
 	this.nodes = [];
-	this.connections = [];
-	this.canvas = canvas_object;
-	this.damping = .000006;
-	var myGraph = this;
-	
-	setInterval(function() { 
-		if(!myGraph.done_rendering) {
-			myGraph.checkRedraw();
-		} else {
-			myGraph.resetNodes();
-		}
-		if(myGraph.canvas.dragging) {
-			myGraph.resetNodes();
-			myGraph.done_rendering = false;
-		}
-	}, 30);
-	// add elements to the canvas object
+	this.edges = [];
+	this.config = [];
+	this.config['canvas'] = c;
+	this.config['origin'] = 
+	this.damping = .000005;
 	this.timestep = 150;
 	this.kineticenergy = 1;
 	this.total_node_velocity = 0;
 	this.done_rendering = false;
+	
+	var g = this;
+	setInterval(function() { 
+		if(!g.done_rendering) {
+			g.checkRedraw();
+		} else {
+			g.resetNodes();
+		}
+		if(g.config['canvas'].dragging) {
+			g.resetNodes();
+			g.done_rendering = false;
+		}
+	}, 30);
 }
-Graph.prototype.addNode = function(node) {
-	this.nodes.push(node);
-	this.canvas.add(node);
+Graph.prototype.addNode = function(n) {
+	this.nodes.push(n);
+	this.config['canvas'].add(n);
 	this.checkRedraw();
 }
-Graph.prototype.addConnection = function(connection) {
-	this.connections.push(connection);
-	this.canvas.add(connection);
+Graph.prototype.addEdge = function(e) {
+	this.edges.push(e);
+	this.config['canvas'].add(e);
 }
 Graph.prototype.resetNodes = function() {
 	for(var i = 0; i < this.nodes.length; i++) {
@@ -48,49 +49,59 @@ Graph.prototype.checkRedraw = function() {
 		node.netforcey = 0;
 		node.velocityx = 0;
 		node.velocityy = 0;
-		if(this.canvas.selection == null || this.canvas.selection != node) {
-			for(var j = 0; j < this.connections.length; j++) {
-				var con = this.connections[j];
+		if(this.config['canvas'].selection == null || this.config['canvas'].selection != node) {
+			for(var j = 0; j < this.edges.length; j++) {
+				var con = this.edges[j];
 				if(con.a == node || con.b == node) {
+					// calculate the attractive force between nodes
 					var other_node = (con.a == node) ? con.b : con.a;
-					var distance = Math.sqrt(((node.x - other_node.x)*(node.x - other_node.x)) + ((node.y - other_node.y)*(node.y - other_node.y)));
-					var force = .1 * Math.max(distance + 350, 1);
-					node.netforcex += force * Math.sin((other_node.x - node.x)/distance);
-					node.netforcey += force * Math.sin((other_node.y - node.y)/distance);
+					node.applyAttractiveForce(other_node);
+					// calculate the repulsive force between nodes
 					for(var k = 0; k < this.nodes.length; k++) {
 						var rep_node = this.nodes[k];
-						var rep_distance = Math.max(Math.sqrt(((node.x - rep_node.x)*(node.x - rep_node.x)) + ((node.y - rep_node.y)*(node.y - rep_node.y))), 1);
-						// calculate repulsion force between nodes
-						var rep_force = -1 * ((node.charge * rep_node.charge)/(rep_distance*rep_distance));
-						node.netforcex += rep_force * Math.sin((rep_node.x - node.x)/rep_distance);
-						node.netforcey += rep_force * Math.sin((rep_node.y - node.y)/rep_distance);		
+						node.applyRepulsiveForce(rep_node);	
 					}
+					// we eventually want to stop the nodes from moving
 					node.netforcex = (Math.abs(node.netforcex) < 1) ? 0 : node.netforcex;
 					node.netforcey = (Math.abs(node.netforcey) < 1) ? 0 : node.netforcey;
+					// set the velocity of the nodes based on their net force
 					node.velocityx = (node.netforcex == 0) ? 0 : (node.velocityx + this.timestep * node.netforcex) * this.damping;
 					node.velocityy = (node.netforcey == 0) ? 0 : (node.velocityy + this.timestep * node.netforcey) * this.damping;
 				}
 			}	
 		}
+		
+		// move the nodes scaled by constant timestep
 		node.x += node.velocityx * this.timestep;
 		node.y += node.velocityy * this.timestep;
+		// magnitude of the velocity vector
 		var velocity = Math.abs(Math.sqrt((node.velocityx*node.velocityx) + (node.velocityy*node.velocityy)));
+		// keep track of the net velocity of the entire system
 		this.total_node_velocity += velocity;
+		
 		this.kineticenergy += node.mass * (velocity*velocity);
 	}	
 	if(this.total_node_velocity == 0) {
-		this.canvas.valid = true;
+		this.config['canvas'].valid = true;
 		this.done_rendering = true;
 	} else {
-		this.canvas.valid = false;
+		this.config['canvas'].valid = false;
 		this.done_rendering = false;
 	}
 }
 
+/*
+ * Node
+ */
 function Node(label, charge) {
-	this.x = Math.floor(Math.random()*1000);
-	this.y = Math.floor(Math.random()*1000);
+	this.position = new Point(
+		Math.floor(Math.random()*1000),
+		Math.floor(Math.random()*1000)	
+	);
 	Circle.call(this, this.x, this.y, 10);
+	Point.call(this, this.x, this.y);
+	this.x = this.position.x; // makes it easier to access the point
+	this.y = this.position.y; // makes it easier to access the point
 	this.charge = charge || 60;
 	this.mass = 100;
 	this.velocityx = 0;
@@ -100,26 +111,56 @@ function Node(label, charge) {
 	this.label = label;
 }
 Node.prototype = Object.create(new Circle());
+
 Node.prototype.reset = function() {
 	this.velocityx = 0;
 	this.velocityy = 0;
 	this.netforcex = 0;
 	this.netforcey = 0;
 }
+Node.prototype.distance = function(n) {
+	return Math.sqrt(
+		((this.x - n.x)*(this.x - n.x)) + 
+		((this.y - n.y)*(this.y - n.y))
+	);
+}
+Node.prototype.applyAttractiveForce = function(n) {
+	var distance = this.distance(n);
+	var force = .1 * Math.max(distance + 350, 1);
+	this.netforcex += force * Math.sin((n.x - this.x)/distance);
+	this.netforcey += force * Math.sin((n.y - this.y)/distance);
+}
+Node.prototype.applyRepulsiveForce = function(n) {
+	var d = Math.max(this.distance(n), 1);
+	// calculate repulsion force between nodes
+	var f = -1 * ((this.charge * n.charge) / (d * d));
+	this.netforcex += f * Math.sin((n.x - this.x)/d);
+	this.netforcey += f * Math.sin((n.y - this.y)/d);	
+}
 
-function Connection(node_a, node_b) {
-	Line.call(this, node_a.x, node_a.y, node_b.x, node_b.y);
-	this.a = node_a;
-	this.b = node_b;
-	this.line = new Line(node_a.x, node_b.y);
+function Edge(a, b) {
+	Line.call(this, a.x, a.y, b.x, b.y);
+	this.a = a;
+	this.b = b;
 	this.draggable = false;
 }
-Connection.prototype = Object.create(new Line());
+Edge.prototype = Object.create(new Line());
 // we need to override the draw method so it updates on a redraw
-Connection.prototype.draw = function(ctx) {
+Edge.prototype.draw = function(ctx) {
 	ctx.strokeStyle = "#B2B2B2";
 	ctx.beginPath();
 	ctx.moveTo(this.a.x, this.a.y);
 	ctx.lineTo(this.b.x, this.b.y);
 	ctx.stroke();
+}
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+Point.prototype.distance = function(n) {
+	return Math.sqrt(
+		((this.x - n.x)*(this.x - n.x)) + 
+		((this.y - n.y)*(this.y - n.y))
+	);
 }
